34bc3ba5e96c062ff9d3aecd9879581a1a58e4d9
youyo <1003ni2@gmail.com>
2018-08-30 12:27:05 +0900

    rename and add get-screen-with-no-host-registration()

--- pabbic.py
+++ pabbic.py
@@ -11,6 +11,23 @@ class Pabbic():
     def __init__(self):
         pass
 
+    def get_screen_with_no_host_registration(self, format='json'):
+        response = {
+            'count': 0
+        }
+        params = {'output': 'extend', 'sortfield': 'screenid'}
+        zapi = self.__auth()
+        resp = self.__request(zapi, 'screen.get', params)
+        for result in resp['result']:
+            params = {'output': 'extend', 'screenids': result['screenid'], 'countOutput': True}
+            resp = self.__request(zapi, 'screenitem.get', params)
+            if resp['result'] == '0':
+                response['count'] += 1
+        if format == 'text':
+            print(response['count'])
+        else:
+            print(json.dumps(response))
+
     def get_disabled_host(self, format='json'):
         params = {'filter': {'status': 1}, 'output': 'extend'}
         zapi = self.__auth()
